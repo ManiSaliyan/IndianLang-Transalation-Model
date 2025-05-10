6ca1f595cc52e1f44499be9430968a2a75114ca7
ManiSaliyan <138963170+ManiSaliyan@users.noreply.github.com>
2025-05-10 16:03:29 +0530

    Create trans.py

---
+++ trans.py
@@ -0,0 +1,21 @@
+#!pip install transformers -U -q
+#! pip install sentencepiece
+#! pip install sentencepiece
+
+from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+
+model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-one-to-many-mmt")
+tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-one-to-many-mmt", src_lang="en_XX")
+print(tokenizer.lang_code_to_id.keys())
+def indianTrans(text,lang):
+    model_inputs = tokenizer(text, return_tensors="pt")
+    generated_tokens = model.generate(
+    **model_inputs,
+    forced_bos_token_id=tokenizer.lang_code_to_id[lang])
+    translation = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    return translation
+
+res = indianTrans("My name is Manikantan","ml_IN")
+print(res)
+
+#dict_keys(['ar_AR', 'cs_CZ', 'de_DE', 'en_XX', 'es_XX', 'et_EE', 'fi_FI', 'fr_XX', 'gu_IN', 'hi_IN', 'it_IT', 'ja_XX', 'kk_KZ', 'ko_KR', 'lt_LT', 'lv_LV', 'my_MM', 'ne_NP', 'nl_XX', 'ro_RO', 'ru_RU', 'si_LK', 'tr_TR', 'vi_VN', 'zh_CN', 'af_ZA', 'az_AZ', 'bn_IN', 'fa_IR', 'he_IL', 'hr_HR', 'id_ID', 'ka_GE', 'km_KH', 'mk_MK', 'ml_IN', 'mn_MN', 'mr_IN', 'pl_PL', 'ps_AF', 'pt_XX', 'sv_SE', 'sw_KE', 'ta_IN', 'te_IN', 'th_TH', 'tl_XX', 'uk_UA', 'ur_PK', 'xh_ZA', 'gl_ES', 'sl_SI'])
